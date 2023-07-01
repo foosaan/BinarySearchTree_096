6d5a3fc19829a8c17d57137786da2bd2c5d59ed8
foosaan <fauzan4297@gmail.com>
2023-07-01 09:19:42 +0700

    assign value to the data field of the new node

--- binary search tree/binary search tree.cpp
+++ binary search tree/binary search tree.cpp
@@ -30,26 +30,5 @@ public:
 	{
 		node* newnode = new node(element, NULL, NULL); //Allocate memory for the new node
 		newnode->info = element; //Assign value to the data field of the new node
-		newnode->leftchild = NULL; //Make the left child of the new node point to NULL
-		newnode->rightchild = NULL; //Make the right child of the new node point to NULL
-
-		node* parent = NULL;
-		node* currentnode = NULL;
-		search(element, parent, currentnode); //Locate the node which will be the parent of the node to be inserted
-
-		if (parent == NULL) //if the parent is NULL (tree is empty)
-		{
-			ROOT = newnode; // Mark the new node as ROOT
-			return; //exit
-		}
-
-		if (element < parent->info)  //if the value in the data field of the new node is less than that of the paren
-		{
-			parent->leftchild = newnode; //Make the left child of the parent point to the new node
-		}
-		else if (element > parent->info) // If the value in the data field of the new node is greater than that of the parent
-		{
-			parent->rightchild = newnode; //Make the right child of the parent point to the new node
-		}
-	}
+		
 };
